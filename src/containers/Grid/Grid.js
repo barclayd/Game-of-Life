571c5e234dd72f744aee0bb8300e0fe0247c74fb
Daniel Barclay <barclaysd@me.com>
2019-02-06 21:27:01 +0000

    30x50 grid of <Box /> with hover property displayed

--- src/containers/Grid/Grid.js
+++ src/containers/Grid/Grid.js
@@ -1,12 +1,37 @@
 import React, {Component} from 'react';
+import Box from '../../components/Box/Box';
 import * as classes from './Grid.module.css';
 
 class Grid extends Component {
 
+    selectBoxHandler = (row, col) => {
+        // pass in row and col to function
+    };
+
     render() {
+        let rowsArray = [];
+        let boxClass = "";
+        for (let i = 0; i < this.props.rows; i++) {
+            for (let j=0; j < this.props.columns; j++) {
+                let boxId = i + "_" + j;
+                boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+                rowsArray.push(
+                    <Box
+                        boxClass={boxClass}
+                        boxId={boxId}
+                        key={boxId}
+                        row={i}
+                        column={j}
+                        selectBox={this.selectBoxHandler} />
+                )
+            }
+        }
+
+        const width = this.props.columns * 16;
+
         return (
-            <div className={classes.grid}>
-                <p>Grid goes here!</p>
+            <div className={classes.grid} style={{width: width}}>
+                {rowsArray}
             </div>
         )
     }
